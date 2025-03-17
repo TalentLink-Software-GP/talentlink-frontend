097c3edaa8ae5787ea5c137ea0ef06a990764eb9
ahmadmsaadeh <asssa72412@gmail.com>
2025-03-17 17:17:21 +0200

    Edit Home Screen For User

--- talent_link/lib/widgets/home_page.dart
+++ talent_link/lib/widgets/home_page.dart
@@ -9,20 +9,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const Center(child: Text("Home Screen")),
+    const Center(child: Text("Jobs Screen")),
+    const Center(child: Text("Maps Screen")),
+    const Center(child: Text("Profile Screen")),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      bottomNavigationBar: BottomAppBar(
-        color: const Color(0xFF0C9E91),
-        child: Row(
-          children: [
-            ElevatedButton(onPressed: () {}, child: Text("Home")),
-            ElevatedButton(onPressed: () {}, child: Text("Home")),
-            ElevatedButton(onPressed: () {}, child: Text("Home")),
-            ElevatedButton(onPressed: () {}, child: Text("Home")),
-          ],
+      appBar: AppBar(
+        title: Center(child: const Text("Talent Link")),
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.w900,
+          color: Colors.black,
+          fontSize: 23,
+        ), // Change title as needed
+        leading: IconButton(
+          icon: const Icon(Icons.message), // Messages icon (top left)
+          onPressed: () {
+            // Handle messages click
+          },
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.notifications,
+            ), // Notifications icon (top right)
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: IndexedStack(index: _selectedIndex, children: _pages),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.teal,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.find_in_page),
+            label: "Jobs",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.map_outlined), label: "Map"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
       ),
     );
   }
