eed05654b7109ce9ee97e4f1a58f23d29518bde9
Ahmed Moeen Awwad <124081794+ahmed-awwad00@users.noreply.github.com>
2025-04-29 21:30:29 +0300

    messageDeleting+messageRealtime (#35)

---
+++ talent_link/lib/widgets/after_login_pages/home_page_tabs/profile_tab_sections/messageNotifications.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MessageNotification extends StatelessWidget {
+  final int count;
+
+  const MessageNotification({required this.count});
+
+  @override
+  Widget build(BuildContext context) {
+    final int countTest = 5;
+    // count = countTest;
+    return countTest > 0
+        ? Container(
+          padding: EdgeInsets.all(6),
+          decoration: BoxDecoration(
+            color: Colors.redAccent,
+            shape: BoxShape.circle,
+          ),
+          child: Text(
+            '$countTest',
+            style: TextStyle(color: Colors.white, fontSize: 12),
+          ),
+        )
+        : SizedBox();
+  }
+}
